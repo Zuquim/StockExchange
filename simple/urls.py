ea0ceb238d2f799c847bf8ac299873c2cce866d0
Marco Túlio Zuquim <tulio@century.net.br>
2019-05-07 19:34:45 -0300

    Inserting 3 view paths

--- simple/urls.py
+++ simple/urls.py
@@ -3,4 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('offers', views.offers, name='offers'),
+    path('offer/<int:offer_id>', views.offer_details, name='offer_details'),
+    path('make_offer', views.make_offer, name='make_offer'),
 ]
