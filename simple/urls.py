148b19ebb78740f25e80ec710a704f4dc8024f5e
Marco Túlio Zuquim <tulio@century.net.br>
2019-05-07 21:20:13 -0300

    FIX: offer_details path

--- simple/urls.py
+++ simple/urls.py
@@ -4,6 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('offers', views.offers, name='offers'),
-    path('offer/<int:offer_id>', views.offer_details, name='offer_details'),
+    path('offer_details/<int:offer_id>', views.offer_details, name='offer_details'),
     path('make_offer', views.make_offer, name='make_offer'),
 ]
